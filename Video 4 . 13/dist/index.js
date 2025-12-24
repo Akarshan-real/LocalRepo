fd4c0613e805484365c2629833990cc34751feac
Akarshan Chakraborty <akarshanchakraborty@gmail.com>
2025-12-24 09:47:57 +0530

    1st commit

---
+++ Video 4 . 13/dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function greet(name) {
+    return `Hello ${name} , welcome to chai code`;
+}
+const username = "Chai aur typescript";
+console.log(greet(username));
+//# sourceMappingURL=index.js.map
---
+++ Video 6 . 11/dist/unionAndAny.js
@@ -0,0 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let subs = '1M';
+let apiReqStatus = "pending";
+apiReqStatus = "success";
+let airlineSeat = "middle";
+airlineSeat = "window";
+const orders = ['12', '34', '52', '76'];
+let currentOrder;
+for (let order of orders) {
+    if (order === '52') {
+        currentOrder = order;
+    }
+    currentOrder = "11";
+}
+console.log(currentOrder);
+//# sourceMappingURL=unionAndAny.js.map
