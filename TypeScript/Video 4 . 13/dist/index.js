e28595c12421c0e1c377612f0b9c7b7ef1903f78
Akarshan Chakraborty <akarshanchakraborty@gmail.com>
2025-12-28 15:19:05 +0530

    change location

--- TypeScript/Video 4 . 13/dist/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function greet(name) {
-    return `Hello ${name} , welcome to chai code`;
-}
-const username = "Chai aur typescript";
-console.log(greet(username));
-//# sourceMappingURL=index.js.map
