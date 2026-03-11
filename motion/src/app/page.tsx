242d44ca630bcb9254976f1bdae10743bd40963b
Akarshan Chakraborty <akarshanchakraborty@gmail.com>
2026-03-11 13:03:54 +0530

    pc

--- motion/src/app/page.tsx
+++ motion/src/app/page.tsx
@@ -4,8 +4,11 @@ import Layout from "@/components/Dashboard/Layout";
 import Navbar from "@/components/Navbar/Navbar";
 import Card from "@/components/Songs/Card";
 import MotionHookExample from "@/components/ui/motion-hook-example";
+import AnimatedText from "../components/AnimatedText/AnimatedText";
 export default function Home() {
   return (
-      <Navbar />
+    <div className="flex items-center justify-center min-h-screen bg-black/98">
+      <AnimatedText />
+    </div>
   );
 }
---
+++ motion/src/components/AnimatedText/AnimatedText.tsx
@@ -0,0 +1,54 @@
+"use client";
+import { motion, stagger, useAnimate } from 'motion/react';
+import { useEffect } from 'react';
+
+const AnimatedText = () => {
+    const text = "Welcome to my portfolio! I'm a passionate developer with experience in React, Node.js, and more. I love creating interactive and dynamic web applications that provide great user experiences. Feel free to explore my projects and get in touch if you'd like to collaborate!";
+
+    const [scope, animate] = useAnimate();
+
+    function startAnimate() {
+        animate("span", {
+            opacity: 1,
+            filter: "blur(0px)",
+            y: 0,
+        }, {
+            duration: 0.5,
+            ease: "easeInOut",
+            delay: stagger(0.02)
+        })
+    };
+
+    // useEffect(() => {
+    //     startAnimate();
+    // }, []); // for auto start at page boot up, you can remove this if you want to start animation on button click only
+    
+    return (
+        <div
+            ref={scope}
+            className='max-w-4xl mx-auto text-4xl text-white/85'
+        >
+            <button
+                onClick={startAnimate}
+                className='bg-neutral-800 px-4 py-2 rounded-md cursor-pointer active:scale-95 transition block mb-4'
+            >
+                Who is Akarshan?
+            </button>
+            {text.split(" ").map((word, index) => (
+                <motion.span
+                    key={word + index}
+                    style={{
+                        y: 10,
+                        opacity: 0,
+                        filter: "blur(10px)"
+                    }}
+                    className='inline-block'
+                >
+                    {word} &nbsp;
+                </motion.span>
+            ))}
+        </div>
+    );
+};
+
+export default AnimatedText;
