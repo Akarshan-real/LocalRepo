7ba3504ddaae620ba56a63111af8c53fa98695c8
Akarshan Chakraborty <akarshanchakraborty@gmail.com>
2026-03-17 20:16:34 +0530

    extension problem git fixed

---
+++ chrome-extension/youtube-bookmarker-starter-code/contentScript.js
@@ -0,0 +1,64 @@
+(() => {
+    let youtubeLeftControls, youtubePlayer;
+    let currentVideo = "";
+    let currentVideoBookmarks = [];
+
+    chrome.runtime.onMessage.addListener((obj, sender, res) => {
+        const { type, value, videoId } = obj;
+
+        if (type === "NEW") {
+            currentVideo = videoId;
+            newVideoLoaded();
+        }
+    });
+
+    const fetchBookmarks = async () => {
+        return new Promise((resolve) => {
+            chrome.storage.sync.get(currentVideo, (data) => {
+                resolve(data[currentVideo] ? JSON.parse(data[currentVideo]) : []);
+            });
+        });
+    };
+
+    const newVideoLoaded = async () => {
+        const bookMarkButtonExist = document.getElementsByClassName("bookmark-btn")[0];
+        currentVideoBookmarks = await fetchBookmarks();
+
+        if (!bookMarkButtonExist) {
+            const bookMarkButton = document.createElement("img");
+
+            bookMarkButton.src = chrome.runtime.getURL("assets/bookmark.png");
+            bookMarkButton.className = "ytp-button " + "bookmark-btn";
+            bookMarkButton.title = "Click to bookmar current timestamp";
+
+            youtubeLeftControls = document.getElementsByClassName("ytp-left-controls")[0];
+            youtubePlayer = document.getElementsByClassName("video-steam")[0];
+
+            youtubeLeftControls.appendChild(bookMarkButton);
+            bookMarkButton.addEventListener("click", addNewBookmarkEventHandler);
+        };
+    };
+
+    const addNewBookmarkEventHandler = async () => {
+        const currentTime = youtubePlayer.currentTime;
+        const newBookmark = {
+            time: currentTime,
+            description: "Bookmark at " + getTime(currentTime),
+        };
+
+        currentVideoBookmarks = await fetchBookmarks();
+
+        console.log(newBookmark);
+
+        chrome.storage.sync.set({
+            [currentVideo]: JSON.stringify([...currentVideoBookmarks, newBookmark].sort((a, b) => a.time - b.time))
+        })
+    }
+})();
+
+const getTime = t => {
+    var date = new Date(0);
+    date.setSeconds(t);
+
+    return date.toISOString.substr(11, 8);
+};
---
+++ chrome-extension/youtube-bookmarker-starter-code/popup.js
@@ -0,0 +1,48 @@
+import { getActiveTabURL } from "./utils";;
+
+// adding a new bookmark row to the popup
+const addNewBookmark = (bookmarkElement, bookmark) => {
+    
+};
+
+const viewBookmarks = (currentVideoBookmarks = []) => {
+    const bookmarkElement = document.getElementsByClassName("bookmark-list")[0];
+    bookmarkElement.innerHTML = "";
+
+    if (currentVideoBookmarks.length > 0) {
+        for (let i = 0; i < currentVideoBookmarks.length; i++) {
+            const bookmark = currentVideoBookmarks[i];
+
+            addNewBookmark(bookmarkElement, bookmark);
+        };
+    }
+    else {
+        bookmarkElement.innerHTML = `<i class="title">No bookmarks for this video yet. Start adding some!</i>`;
+    }
+};
+
+const onPlay = e => { };
+
+const onDelete = e => { };
+
+const setBookmarkAttributes = () => { };
+
+document.addEventListener("DOMContentLoaded", async () => {
+    const activeTabURL = await getActiveTabURL();
+    const queryParameters = activeTabURL.split("?")[1];
+    const urlParams = new URLSearchParams(queryParameters);
+
+    const currentVideo = urlParams.get("v");
+
+    if (activeTabURL.url.includes("youtube.com/watch") && currentVideo) {
+        chrome.storage.sync.get([currentVideo], (data) => {
+            const currentVideoBookmarks = data[currentVideo] ? JSON.parse(data[currentVideo]) : [];
+
+            viewBookmarks(currentVideoBookmarks);
+        });
+    }
+    else {
+        const container = document.getElementsByClassName("container")[0];
+        container.innerHTML = `<h2 class="title">This is not a YouTube video page.</h2>`;
+    };
+});
